15fdd2a2888cb1a5f81302489964e304b2794929
thulasiramegowda <thulasiramegowda29@gmail.com>
2026-06-06 12:50:56 +0530

    Added rectangle drawing

---
+++ GraphicsEditor/README.md
@@ -0,0 +1,15 @@
+# 2D Graphics Editor
+
+A simple 2D graphics editor implemented in C using a 2D character array.
+
+Features:
+- Draw Rectangle
+- Draw Line
+- Draw Circle
+- Draw Triangle
+- Add Shapes
+- Delete Shapes
+- Modify Shapes
+- Display Canvas
+
+Author: Thulasi Rame Gowda
---
+++ GraphicsEditor/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+#define ROWS 10
+#define COLS 20
+
+char canvas[ROWS][COLS];
+
+void initCanvas()
+{
+    for(int i=0;i<ROWS;i++)
+    {
+        for(int j=0;j<COLS;j++)
+        {
+            canvas[i][j]='_';
+        }
+    }
+}
+
+void displayCanvas()
+{
+    for(int i=0;i<ROWS;i++)
+    {
+        for(int j=0;j<COLS;j++)
+        {
+            printf("%c",canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void drawRectangle(int x, int y, int width, int height)
+{
+    int i, j;
+
+    for(i = y; i < y + height; i++)
+    {
+        for(j = x; j < x + width; j++)
+        {
+            if(i == y || i == y + height - 1 ||
+               j == x || j == x + width - 1)
+            {
+                canvas[i][j] = '*';
+            }
+        }
+    }
+}
+
+int main()
+{
+    initCanvas();
+
+    drawRectangle(2, 2, 10, 5);
+
+    displayCanvas();
+
+    return 0;
+}
